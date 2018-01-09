17eaf32652c359aaf53851455dd1e207253a5f3f
MikeBLambert <mlamb128@gmail.com>
2018-01-09 06:55:49 -0800

    add words and functionality for definitions to appear when words are clicked on.

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+
+$(function() {
+  $(".word#jsWord").click(function() {
+    $(".definition").hide();
+    $(".definition#jsDef").toggle();
+  });
+});
+
+
+$(function() {
+  $(".word#operWord").click(function() {
+    $(".definition").hide();
+    $(".definition#operDef").toggle();
+  });
+});
+
+$(function() {
+  $(".word#varWord").click(function() {
+    $(".definition").hide();
+    $(".definition#varDef").toggle();
+  });
+});
+
+$(function() {
+  $(".word#varNamConWord").click(function() {
+    $(".definition").hide();
+    $(".definition#varNamConDef").toggle();
+  });
+});
+
+$(function() {
+  $(".word#funWord").click(function() {
+    $(".definition").hide();
+    $(".definition#funDef").toggle();
+  });
+});
+
+$(function() {
+  $(".word#metWord").click(function() {
+    $(".definition").hide();
+    $(".definition#metDef").toggle();
+  });
+});
+
+$(function() {
+  $(".word#argWord").click(function() {
+    $(".definition").hide();
+    $(".definition#argDef").toggle();
+  });
+});
+
+$(function() {
+  $(".word#parWord").click(function() {
+    $(".definition").hide();
+    $(".definition#parDef").toggle();
+  });
+});
+
+$(function() {
+  $(".word#retWord").click(function() {
+    $(".definition").hide();
+    $(".definition#retDef").toggle();
+  });
+});
+
+$(function() {
+  $(".word#chaMetWord").click(function() {
+    $(".definition").hide();
+    $(".definition#chaMetDef").toggle();
+  });
+});
